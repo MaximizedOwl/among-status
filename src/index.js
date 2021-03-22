ca40864221c630316bd7cba16648e132ae7a4cca
MaximizedOwl <mkarasawaaa+0@gmail.com>
2021-03-22 14:12:26 +0900

    使っていないモジュールの記述をコメントアウト

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import CssBaseline from '@material-ui/core/CssBaseline';
-import { ThemeProvider } from '@material-ui/core/styles';
-import App from './App';
-import theme from './theme';
+// import CssBaseline from '@material-ui/core/CssBaseline';
+// import { ThemeProvider } from '@material-ui/core/styles';
+// import App from './App';
+// import theme from './theme';
 import Paperbase from './paperbase/Paperbase';
 
 ReactDOM.render(
