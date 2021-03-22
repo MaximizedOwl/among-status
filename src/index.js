d4dd13cd04e72b16638f422315ba159f464efa90
MaximizedOwl <mkarasawaaa+0@gmail.com>
2021-03-22 11:54:30 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
+import App from './App';
+import theme from './theme';
+import Paperbase from './paperbase/Paperbase';
+
+ReactDOM.render(
+
+  <Paperbase />,
+  // <ThemeProvider theme={theme}>
+  //   {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+  //   <CssBaseline />
+  //   <App />
+  // </ThemeProvider>,
+  document.querySelector('#root'),
+);
