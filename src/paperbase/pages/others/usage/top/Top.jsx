2399bfb65ed1deec87bb847d06cf6adff99411c1
MaximizedOwl <mkarasawaaa+0@gmail.com>
2021-04-17 16:17:20 +0900

    importしているが使っていないものを削除

--- src/paperbase/pages/others/usage/top/Top.jsx
+++ src/paperbase/pages/others/usage/top/Top.jsx
@@ -1,12 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import AppBar from '@material-ui/core/AppBar';
 import Paper from '@material-ui/core/Paper';
 import { withStyles } from '@material-ui/core/styles';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import TabPanel from '../../../../components/TabPanel';
-import { Toolbar, Typography } from '@material-ui/core';
 import Basic from './basic/Basic';
 
 const styles = (theme) => ({
