c52e524496fb9d629c206351db1c0146bceec4d8
MaximizedOwl <mkarasawaaa+0@gmail.com>
2021-04-23 08:46:03 +0900

    利用していない記述を削除

--- src/paperbase/components/Header.jsx
+++ src/paperbase/components/Header.jsx
@@ -15,12 +15,10 @@ import MuiDialogContent from '@material-ui/core/DialogContent';
 import MuiDialogActions from '@material-ui/core/DialogActions';
 import Snackbar from '@material-ui/core/Snackbar';
 import Alert from './Alert';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
 import CloseIcon from '@material-ui/icons/Close';
 import TwitterIcon from '@material-ui/icons/Twitter';
 import LinkIcon from '@material-ui/icons/Link';
 import { withStyles } from '@material-ui/core/styles';
-import { useTheme } from '@material-ui/core/styles';
 
 // const lightColor = 'rgba(255, 255, 255, 0.7)';
 
@@ -41,19 +39,6 @@ const styles = (theme) => ({
     top: theme.spacing(1),
     color: theme.palette.grey[500],
   },
-  // iconButtonAvatar: {
-  //   padding: 4,
-  // },
-  // link: {
-  //   textDecoration: 'none',
-  //   color: lightColor,
-  //   '&:hover': {
-  //     color: theme.palette.common.white,
-  //   },
-  // },
-  // button: {
-  //   borderColor: lightColor,
-  // },
 });
 
 const DialogTitle = withStyles(styles)((props) => {
@@ -133,10 +118,6 @@ function Header(props) {
       console.log('failure');
     });
   };
-
-  // シェアのダイアログの領域の調整に関して
-  const theme = useTheme();
-  const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
 
   return (
     <React.Fragment>
